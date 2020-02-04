e3c0502254744d7ab14992d4cd268293843b7cb7
Sinth-Lord <60325195+Sinth-Lord@users.noreply.github.com>
2020-02-04 08:09:16 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,85 @@
 #include "project.h"
 
-//uint8 Status; 
-uint16 byte_1;
-uint16 byte_2;
-uint16 bit_Mask = 0xFFF0;
+#define MIDI_Buffer_Size 4
+int MIDI_Buffer[MIDI_Buffer_Size] = {0,0,0,0};
+
+int bufferLength = 1;
+int writeIndex = 0;
+
+CY_ISR(MIDI_Interrupt)
+{
+    MIDI_Buffer[writeIndex] = UART_GetChar();
+    writeIndex = (writeIndex + 1)&0x3;
+    bufferLength = (bufferLength + 1)&0x3;
+}
+
+uint8 byte_1;  //Note on/off
+uint8 byte_2;  //Note number
+uint8 byte_3;  //Velocity
+uint8 bit_Mask = 0xF0;
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    UART_Start();
-    UART_Clock_Start();
 
+    Clock_Start();
+    UART_Start();
+    Osc_Freq_Start();
+    
+    //interrupt initialization
+    
+    MIDI_Interrupt_Start();
+    MIDI_Interrupt_StartEx(MIDI_Interrupt);
+    MIDI_Interrupt_Enable();
+    
     for(;;)
     {
-        // Get byte 1 (status message)
-        byte_1 = UART_GetByte();
+        /* Place your application code here. */
         
-        // Note-On Messages
-        if ((byte_1 & bit_Mask) == 0x90u)
+        // Note on / Note off
+        byte_1 = MIDI_Buffer[bufferLength];
+        byte_2 = MIDI_Buffer[((bufferLength + 1)&0x3)];
+        byte_3 = MIDI_Buffer[((bufferLength + 2)&0x3)];
+        switch((byte_1 & bit_Mask))
         {
-            UART_ClearRxBuffer();
-            byte_2 = UART_GetByte();
-            
-            if (byte_2 == 60u)
+            case 0x80: Osc_Freq_StopBlock();
+            break;
+            case 0x90: Osc_Freq_Start();
+            switch(byte_2)
             {
-                LED_Write(1);    
+                case 60u: Osc_Freq_SetDivider(0x1AE7);
+                break;
+                case 61u: Osc_Freq_SetDivider(0x1964);
+                break;
+                case 62u: Osc_Freq_SetDivider(0x17F7);
+                break;
+                case 63u: Osc_Freq_SetDivider(0x169F);
+                break;
+                case 64u: Osc_Freq_SetDivider(0x155A);
+                break;
+                case 65u: Osc_Freq_SetDivider(0x1427);
+                break;
+                case 66u: Osc_Freq_SetDivider(0x1305);
+                break;
+                case 67u: Osc_Freq_SetDivider(0x11F4);
+                break;
+                case 68u: Osc_Freq_SetDivider(0x10F2);
+                break;
+                case 69u: Osc_Freq_SetDivider(0x0FFE);
+                break;
+                case 70u: Osc_Freq_SetDivider(0x0F19);
+                break;
+                case 71u: Osc_Freq_SetDivider(0x0E40);
+                break;
+                default: Osc_Freq_SetDivider(0x0000);
+                break;
             }
-            else
-            {
-                LED_Write(0);    
-            }
+            break;
         }
-        
-        //Note_Off Messages
-        else if ((byte_1 & bit_Mask) == 0x80u)
-        {
-            LED_Write(0);
-        }
-                
-//        if((Status &= z) == 0x90u)
-//        {
-//            LED_Write(1);   
-//        }
-//        else if((Status &= z) == 0x80u)
-//        {
-//            LED_Write(0);    
-//        }
     }
 }
 
 /* [] END OF FILE */
+
